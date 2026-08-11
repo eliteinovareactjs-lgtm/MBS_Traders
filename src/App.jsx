9041a3cc9396081012194c971e3a1d8e597d238c
eliteinovareactjs-lgtm <eliteinovareactjs@gmail.com>
2026-08-11 15:31:56 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about-page" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
