7f6773613a12c58c2fe411737682f9b1c4e280e7
eliteinovareactjs-lgtm <eliteinovareactjs@gmail.com>
2026-08-11 15:22:36 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// import About from "./pages/AboutPage";
+import About from "./pages/AboutPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
